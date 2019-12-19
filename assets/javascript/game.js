c5a3e9ad4c4efa286b88f6ed5fffb08840313a0c
purdyjen <purdyjen@me.com>
2019-12-19 16:28:12 -0600

    updating for tutoring session

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -38,59 +38,59 @@ document.addEventListener('keypress', (event) => {
             }
         }
           
-        // for (j = 0; j < currentWord.length; j++){
-        //     if (j < guess){
-        //         underscore[currentWord.indexOf(guess)] = guess;
-        //     }
-        //     // break;
-        // }
+        while (index = 0; index < currentWord.length; index++){
+            if ([index] <= guess){
+                underscore[currentWord.indexOf(guess)] = guess;
+            }
+            // break;
+        }
         //replace underscore
-        underscore[currentWord.indexOf(guess)] = guess;
-        if (1 < currentWord.length) {
-        underscore[currentWord.indexOf(guess, 1)] = guess;
-    }
-        if (2 < currentWord.length) {
-        underscore[currentWord.indexOf(guess, 2)] = guess;
-    }
-        if (3 < currentWord.length) {
-        underscore[currentWord.indexOf(guess, 3)] = guess;
-    }
-        if (4 < currentWord.length) {
-        underscore[currentWord.indexOf(guess, 4)] = guess;
-    }
-        if (5 < currentWord.length) {
-        underscore[currentWord.indexOf(guess, 5)] = guess;
-    }
-        if (6 < currentWord.length) {
-        underscore[currentWord.indexOf(guess, 6)] = guess;
-    }
-        if (7 < currentWord.length) {
-        underscore[currentWord.indexOf(guess, 7)] = guess;
-    }
-        if (8 < currentWord.length) {
-        underscore[currentWord.indexOf(guess, 8)] = guess;
-    }
-        if (9 < currentWord.length) {
-        underscore[currentWord.indexOf(guess, 9)] = guess;
-    }
-        if (10 < currentWord.length) {
-        underscore[currentWord.indexOf(guess, 10)] = guess;
-    }
-        if (11 < currentWord.length) {
-        underscore[currentWord.indexOf(guess, 11)] = guess;
-    }
-        if (12 < currentWord.length) {
-        underscore[currentWord.indexOf(guess, 12)] = guess;
-    }
-        if (13 < currentWord.length) {
-        underscore[currentWord.indexOf(guess, 13)] = guess;
-    }
-        if (14 < currentWord.length) {
-        underscore[currentWord.indexOf(guess, 14)] = guess;
-    }
-        if (15 < currentWord.length) {
-        underscore[currentWord.indexOf(guess, 15)] = guess;
-    }
+    //     underscore[currentWord.indexOf(guess)] = guess;
+    //     if (1 < currentWord.length) {
+    //     underscore[currentWord.indexOf(guess, 1)] = guess;
+    // }
+    //     if (2 < currentWord.length) {
+    //     underscore[currentWord.indexOf(guess, 2)] = guess;
+    // }
+    //     if (3 < currentWord.length) {
+    //     underscore[currentWord.indexOf(guess, 3)] = guess;
+    // }
+    //     if (4 < currentWord.length) {
+    //     underscore[currentWord.indexOf(guess, 4)] = guess;
+    // }
+    //     if (5 < currentWord.length) {
+    //     underscore[currentWord.indexOf(guess, 5)] = guess;
+    // }
+    //     if (6 < currentWord.length) {
+    //     underscore[currentWord.indexOf(guess, 6)] = guess;
+    // }
+    //     if (7 < currentWord.length) {
+    //     underscore[currentWord.indexOf(guess, 7)] = guess;
+    // }
+    //     if (8 < currentWord.length) {
+    //     underscore[currentWord.indexOf(guess, 8)] = guess;
+    // }
+    //     if (9 < currentWord.length) {
+    //     underscore[currentWord.indexOf(guess, 9)] = guess;
+    // }
+    //     if (10 < currentWord.length) {
+    //     underscore[currentWord.indexOf(guess, 10)] = guess;
+    // }
+    //     if (11 < currentWord.length) {
+    //     underscore[currentWord.indexOf(guess, 11)] = guess;
+    // }
+    //     if (12 < currentWord.length) {
+    //     underscore[currentWord.indexOf(guess, 12)] = guess;
+    // }
+    //     if (13 < currentWord.length) {
+    //     underscore[currentWord.indexOf(guess, 13)] = guess;
+    // }
+    //     if (14 < currentWord.length) {
+    //     underscore[currentWord.indexOf(guess, 14)] = guess;
+    // }
+    //     if (15 < currentWord.length) {
+    //     underscore[currentWord.indexOf(guess, 15)] = guess;
+    // }
         //testing
         console.log(underscore); //how to replace more than one instance??
        
